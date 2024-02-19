ddd81c7e30a1b4677542af252e757442df52b227
Otávio Francisco <otaviohbf2002@gmail.com>
2024-02-19 04:22:07 -0300

    other: automatic formatation settings

--- backend/src/tests/services/history.service.spec.ts
+++ backend/src/tests/services/history.service.spec.ts
@@ -170,27 +170,24 @@ describe('HistoryService', () => {
   });
 
   it('should return user histories', async () => {
-    // Arrange
+
     const id = '456';
     const mockHistories: HistoryEntity[] = [
       { id: '1', user_id: '456', song_id: '789' },
       { id: '2', user_id: '456', song_id: '890' },
-      { id: '3', user_id: '789', song_id: '123' }, // This one should be filtered out
+      { id: '3', user_id: '789', song_id: '123' }, 
     ];
     const expectedHistories: HistoryModel[] = [
       { id: '1', user_id: '456', song_id: '789' },
       { id: '2', user_id: '456', song_id: '890' },
     ];
 
-    // Mock the getHistories method to return the mockHistories array
     jest.spyOn(mockHistoryRepository, 'getHistories').mockResolvedValue(mockHistories);
 
-    // Act
     const result = await service.getUserHistory(id);
 
-    // Assert
     expect(result).toEqual(expectedHistories);
     expect(mockHistoryRepository.getHistories).toHaveBeenCalledTimes(1);
   });
-  
+
 });
